12ec8ab97f8b6f7f04cec88d472e2d9060dc9c38
“Jen <mpjeno@gmail.com>
2014-09-09 11:24:27 -0500

    Added routes for contact_form

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -1,2 +1,43 @@
 class ContactFormController < ApplicationController
+  
+  def index
+   @forms = ContactForm.all
+  end
+  
+   def new
+    @form = ContactForm.new
+  end
+  
+  def create
+    @form = ContactForm.new(params[:contact_form])
+  
+     if @form.save
+       redirect_to contact_forms_path
+     
+     else
+       render "new"
+     end
+  end
+
+  def edit
+    @form = ContactForm.find(params[:id])
+  end
+
+  def update
+    @form = ContactForm.find(params[:id])
+    if @form.update_attributes(params[:contact_form])
+      render "detail"
+    else
+      render "edit"
+    end
+  end
+
+  def detail
+    @form = ContactForm.find(params[:id])
+  end
+
+  def delete
+    ContactForm.find(params[:id]).delete
+    redirect_to contact_forms_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,30 @@ Profile::Application.routes.draw do
   get "home" => 'pages#intro', as: "home"
   
   # cardinal routes for class_projects controller
-  get "/projects" => 'class_projects#index', :as => "class_projects"
-  get "/projects/new" => 'class_projects#new', :as => "new_project"
-  post "/projects" => 'class_projects#create'
-  get "/projects/:id/edit" => 'class_projects#edit', :as => "edit_project"
-  put "/projects/:id" => 'class_projects#update'
-  get "projects/:id" => 'class_projects#detail', :as => "class_project"
+  get "/projects"            => 'class_projects#index',  :as => "class_projects"
+  get "/projects/new"        => 'class_projects#new',    :as => "new_project"
+  post "/projects"           => 'class_projects#create'
+  get "/projects/:id/edit"   => 'class_projects#edit',   :as => "edit_project"
+  put "/projects/:id"        => 'class_projects#update'
+  get "projects/:id"         => 'class_projects#detail', :as => "class_project"
   post "projects/:id/delete" => 'class_projects#delete'
   
   #cardinal routes for links controller
-  get "/links" => 'links#index', :as => "links"
-  get "/links/new" => 'links#new', :as => "new_link"
-  post "/links" => 'links#create'
-  get "/links/:id/edit" => 'links#edit', :as => "edit_link"
-  put "/links/:id" => 'links#update'
-  get "links/:id" => 'links#detail', :as => "link"
+  get "/links"            => 'links#index',  :as => "links"
+  get "/links/new"        => 'links#new',    :as => "new_link"
+  post "/links"           => 'links#create'
+  get "/links/:id/edit"   => 'links#edit',   :as => "edit_link"
+  put "/links/:id"        => 'links#update'
+  get "links/:id"         => 'links#detail', :as => "link"
   post "links/:id/delete" => 'links#delete'
   
   #cardinal routes for contact_form
-  
+  get "/forms"             => 'forms#index',  :as => "links"
+  get "/forms/new"         => 'forms#new',    :as => "new_form"
+  post "/forms"            => 'forms#create'
+  get "/forms/:id/edit"    => 'forms#edit',   :as => "edit_form"
+  put "/forms/:id"         => 'forms#update'
+  get "/forms/:id"         => 'forms#detail', :as => "form"
+  post "/forms/:id/delete" => 'forms#delete'
   
 end
