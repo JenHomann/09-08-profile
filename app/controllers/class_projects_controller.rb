784cd3921599e6dce3036019897b6e26e632fa35
“Jen <mpjeno@gmail.com>
2014-09-08 22:31:31 -0500

    Built out cardinal routes - all working

--- app/controllers/class_projects_controller.rb
+++ app/controllers/class_projects_controller.rb
@@ -12,11 +12,30 @@ class ClassProjectsController < ApplicationController
       @project = ClassProject.new(params[:class_project])
      
        if @project.save
-         redirect_to class_projects_path 
+         redirect_to class_projects_path
          
        else
          render "new"
        end
+   end
+   
+   def edit
+     @project = ClassProject.find(params[:id])
+   end
+
+   def update
+     @project = ClassProject.find(params[:id])
+     @project.update_attributes({name: params[:project][:name], image: params[:project][:image], week: params[:project][:week], description: params[:project][:description]})
+     render "detail"
+   end
+   
+   def detail
+     @project = ClassProject.find(params[:id])
+   end
+   
+   def delete
+     ClassProject.find(params[:id]).delete
+     redirect_to class_projects_path
    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,41 @@ Profile::Application.routes.draw do
   get "/projects" => 'class_projects#index', :as => "class_projects"
   get "/projects/new" => 'class_projects#new', :as => "new_project"
   post "/projects" => 'class_projects#create'
+  get "/projects/:id/edit" => 'class_projects#edit', :as => "edit_project"
+  post "/projects/:id/edit" => 'class_projects#update', :as => "update_project"
+  get "projects/:id" => 'class_projects#detail', :as => "class_project"
+  post "projects/:id/delete" => 'class_projects#delete'
   
 end
+
+
+#
+# get "/plants/:id/edit" do
+#   @plant = Plant.find(params[:id])
+#
+#   erb :"plants/plant_edit"
+# end
+#
+# post "/plants/:id/update" do
+#   binding.pry
+#   @plant = Plant.find(params[:id])
+#   @plant.update_attributes({name: params[:name], fruitbearing: params[:fruitbearing]})
+#
+#   erb :"plants/plant_update"
+# end
+#
+# post "/plants/:id/delete" do
+#   @plant = Plant.find(params[:id])
+#   @plant.tips.each do |tip|
+#     tip.plants.delete(@plant)
+#   end
+#   @plant.delete
+#
+#   erb :"plants/plant_delete"
+# end
+#
+# get "/plants/:id" do
+#   @plant = Plant.find(params[:id])
+#
+#   erb :"plants/plant_detail"
+# end
