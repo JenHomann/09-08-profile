9456d21e5c39027d8fcd265012274d3e20b48b25
“Jen <mpjeno@gmail.com>
2014-09-10 14:26:16 -0500

    Added home/escape links in all of my views

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,8 @@ class Article < ActiveRecord::Base
   validates :title, :presence => true
   validates :body, :presence => true
   
+  scope :is_draft, where(:draft => true)
+  
   def excerpt
     return body[0...30]
   end
@@ -17,5 +19,9 @@ class Article < ActiveRecord::Base
       "medium"
     end
   end
-
+  
+  def self.long_posts
+    where(:relative_length => "long")
+  end
+  
 end
--- app/models/class_project.rb
+++ app/models/class_project.rb
@@ -2,4 +2,6 @@ class ClassProject < ActiveRecord::Base
   attr_accessible :description, :image, :name, :week, :featured, :code_url, :view_url
   
   validates :name, :presence => true
+  
+  scope :is_featured, where(:featured => true)
 end
