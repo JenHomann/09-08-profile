df1459127afae52feb6ba6ee251b210a3bcd77c6
“Jen <mpjeno@gmail.com>
2014-09-09 20:32:33 -0500

    Created Article model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140909161735) do
+ActiveRecord::Schema.define(:version => 20140910013152) do
+
+  create_table "articles", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "draft"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "class_projects", :force => true do |t|
     t.string   "name"
