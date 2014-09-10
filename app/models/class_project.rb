1e25e170b836cfb1342af5bc573e6315a09ce770
“Jen <mpjeno@gmail.com>
2014-09-09 21:48:19 -0500

    Added :featured attribute to class_projects model

--- app/models/class_project.rb
+++ app/models/class_project.rb
@@ -1,5 +1,5 @@
 class ClassProject < ActiveRecord::Base
-  attr_accessible :description, :image, :name, :week
+  attr_accessible :description, :image, :name, :week, :featured
   
   validates :name, :presence => true
 end
