a9026488da089e782948b3dd1ea666b64bee0172
“Jen <mpjeno@gmail.com>
2014-09-11 13:58:34 -0500

    Added a users table and rake db:migrated.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140911164653) do
+ActiveRecord::Schema.define(:version => 20140911185643) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -63,6 +63,13 @@ ActiveRecord::Schema.define(:version => 20140911164653) do
     t.string   "logo"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
