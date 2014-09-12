c173bfdb109ebae425afcf75f0e9363eba1336da
“Jen <mpjeno@gmail.com>
2014-09-12 05:48:46 -0500

    Added Users controller and updated validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   
   has_secure_password
+  
+  validates :password, :presence => true
+  validates :password, :confirmation => true
 end
