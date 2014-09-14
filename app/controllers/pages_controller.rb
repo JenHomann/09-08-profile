8698e126c10177bc4aed4d3be183041e4bad4f8c
“Jen <mpjeno@gmail.com>
2014-09-14 13:42:05 -0500

    About page, mostly formatted

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   skip_before_filter :authorize
     
   def intro
-    
   end
+  
+  def about
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Profile::Application.routes.draw do
   root :to => 'pages#welcome' #name is already set to "root"
   
   get "home" => 'pages#intro', as: "home"
+  get "about" => 'pages#about', :as => "about"
  
   # cardinal routes for class_projects controller
   resources :class_projects
