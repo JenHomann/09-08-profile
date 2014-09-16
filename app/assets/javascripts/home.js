3f49ac30da58626da4554ad53ec51a2d27d8659c
“Jen <mpjeno@gmail.com>
2014-09-15 22:29:13 -0500

    AUTOPLAY STOPMOTION JQUERY AWESOMENESS!!

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,66 +1,77 @@
 $(document).ready(function(){
   
-  function fireA() {
-    $('#animation').css("background", "url('/images/wave_1.jpg')");
- }
+  $(function() {
 
-  function fireB() {
-   $('#animation').css("background", "url('/images/wave_2.jpg')");
- }
+      $('#animation img:gt(0)').hide(); // to hide all but the first image when page loads
 
-  function fireC() {
-    $('#animation').css("background", "url('/images/wave_3.jpg')");
-  }
-
-  function fireD() {
-    $('#animation').css("background", "url('/images/wave_4.jpg')");
- }
-
-  function fireE() {
-   $('#animation').css("background", "url('/images/wave_5.jpg')");
- }
-
-  function fireF() {
-    $('#animation').css("background", "url('/images/wave_6.jpg')");
-  }
-
-  function fireG() {
-    $('#animation').css("background", "url('/images/wave_7.jpg')");
- }
-
-  function fireH() {
-   $('#animation').css("background", "url('/images/wave_8.jpg')");
- }
-
-  function fireI() {
-    $('#animation').css("background", "url('/images/wave_9.jpg')");
-  }
-
-  function fireJ() {
-    $('#animation').css("background", "url('/images/wave_10.jpg')");
-  }
-
-  function fireK() {
-   $('#animation').css("background", "url('/images/point_1.jpg')");
- }
-
-  function fireL() {
-    $('#animation').css("background", "url('/images/point_2.jpg')");
-  }
-
-  function fireM() {
-    $('#animation').css("background", "url('/images/point_3.jpg')");
-  }
-
-  $(window).scroll(function(){
-    var events = [fireA, fireA, fireB, fireB, fireC, fireC, fireD, fireD, fireE, fireE, fireF, fireF, fireG, fireG, fireH, fireH, fireI, fireI, fireJ, fireJ, fireK, fireK, fireK, fireK, fireK, fireL, fireL, fireL, fireL, fireL, fireM];
-
-    if(!this.counter) { this.counter = 0; }
-
-    events[this.counter]();
-    this.counter++;
- //
- //    // $('#animation').css("background", "url('/images/wave_1.jpg')");
+      setInterval(function()
+      {
+          $('#animation :first-child').fadeOut(1)
+              .next().fadeIn(1).end().appendTo('#animation');
+      },100);
   });
+  
+  
+ //  function fireA() {
+ //    $('#animation').css("background", "url('/images/wave_1.jpg')");
+ // }
+ //
+ //  function fireB() {
+ //   $('#animation').css("background", "url('/images/wave_2.jpg')");
+ // }
+ //
+ //  function fireC() {
+ //    $('#animation').css("background", "url('/images/wave_3.jpg')");
+ //  }
+ //
+ //  function fireD() {
+ //    $('#animation').css("background", "url('/images/wave_4.jpg')");
+ // }
+ //
+ //  function fireE() {
+ //   $('#animation').css("background", "url('/images/wave_5.jpg')");
+ // }
+ //
+ //  function fireF() {
+ //    $('#animation').css("background", "url('/images/wave_6.jpg')");
+ //  }
+ //
+ //  function fireG() {
+ //    $('#animation').css("background", "url('/images/wave_7.jpg')");
+ // }
+ //
+ //  function fireH() {
+ //   $('#animation').css("background", "url('/images/wave_8.jpg')");
+ // }
+ //
+ //  function fireI() {
+ //    $('#animation').css("background", "url('/images/wave_9.jpg')");
+ //  }
+ //
+ //  function fireJ() {
+ //    $('#animation').css("background", "url('/images/wave_10.jpg')");
+ //  }
+ //
+ //  function fireK() {
+ //   $('#animation').css("background", "url('/images/point_1.jpg')");
+ // }
+ //
+ //  function fireL() {
+ //    $('#animation').css("background", "url('/images/point_2.jpg')");
+ //  }
+ //
+ //  function fireM() {
+ //    $('#animation').css("background", "url('/images/point_3.jpg')");
+ //  }
+ //
+ //  $(window).scroll(function(){
+ //    var events = [fireA, fireA, fireB, fireB, fireC, fireC, fireD, fireD, fireE, fireE, fireF, fireF, fireG, fireG, fireH, fireH, fireI, fireI, fireJ, fireJ, fireK, fireK, fireK, fireK, fireK, fireL, fireL, fireL, fireL, fireL, fireM];
+ //
+ //    if(!this.counter) { this.counter = 0; }
+ //
+ //    events[this.counter]();
+ //    this.counter++;
+ //
+ //  });
 
 })
